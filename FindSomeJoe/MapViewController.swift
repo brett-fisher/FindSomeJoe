383461c4cdf028d28cdb411b53427581f6ccef10
Brett Fisher <bdf5106@psu.edu>
2017-10-25 21:54:18 -0400

    Added feature where the app can find you current location when the map view loads.

--- FindSomeJoe/MapViewController.swift
+++ FindSomeJoe/MapViewController.swift
@@ -7,17 +7,38 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
 
+  @IBOutlet weak var mapView: MKMapView!
+  
+  let manager = CLLocationManager()
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    
+    let location = locations[0]
+    
+    let span = MKCoordinateSpanMake(0.01, 0.01)
+    let myLocation = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+    let region = MKCoordinateRegionMake(myLocation, span)
+    
+    mapView.setRegion(region, animated: true)
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    manager.delegate = self
+    manager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+    manager.requestWhenInUseAuthorization()
+    manager.startUpdatingLocation()
+    
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
 
 
