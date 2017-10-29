641665a3012d1b784744e2df04ae6143f9b64f57
Brett Fisher <bdf5106@psu.edu>
2017-10-29 19:46:11 -0400

    Refactored location process to only get your location one time.

--- FindSomeJoe/MapViewController.swift
+++ FindSomeJoe/MapViewController.swift
@@ -21,7 +21,7 @@ class MapViewController: UIViewController {
     
     manager = CLLocationManager()
     manager?.delegate = self
-    manager?.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+    manager?.desiredAccuracy = kCLLocationAccuracyBest
     manager?.requestWhenInUseAuthorization()
     
   }
@@ -29,21 +29,10 @@ class MapViewController: UIViewController {
 
 extension MapViewController: CLLocationManagerDelegate {
   
-  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-    
-    let location = locations[0]
-    
-    let span = MKCoordinateSpanMake(0.01, 0.01)
-    let myLocation = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
-    let region = MKCoordinateRegionMake(myLocation, span)
-    
-    mapView.setRegion(region, animated: true)
-  }
-  
   // Only start updating the location after the user has authorized
   func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
     if status == .authorizedWhenInUse || status == .authorizedAlways {
-      manager.startUpdatingLocation()
+      manager.requestLocation()
     }
   }
   
